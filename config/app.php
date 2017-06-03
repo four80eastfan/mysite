a58bc5c551be53805b9b3fda9078218f46bda343
four80eastfan <matthew.akkerman@gmail.com>
2017-06-03 15:33:47 -0700

    file changes

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'mattakkerman.com'),
+    'url' => env('APP_URL', 'https://mattakkerman.com'),
 
     /*
     |--------------------------------------------------------------------------
